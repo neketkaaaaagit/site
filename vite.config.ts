b7591ec4915dc62ea6ed9c61e45574df303347ad
neketkaaaaagit <167111881+neketkaaaaagit@users.noreply.github.com>
2025-01-19 11:15:32 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { configDefaults } from 'vitest/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/our-shop/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
